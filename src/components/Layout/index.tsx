60bab85d6b0ebd530a793c4475e7ab37f0933571
amrsamir <amrsamir.official@gmail.com>
2025-02-06 21:55:26 +0200

    first commit

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,27 +1,30 @@
-"use client";
-
-import { ReactLenis } from "@studio-freight/react-lenis";
 import { useState } from "react";
+import { animateScroll as scroll } from "react-scroll"; // استيراد react-scroll
 import { Footer, Header, Preloader } from "..";
 import StyledComponentsRegistry from "../../../libs/registry";
 import { GlobalStyles } from "./GlobalStyles";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   const [complete, setComplete] = useState(false);
+
+  // دالة للتمرير إلى الأعلى
+  const scrollToTop = () => {
+    scroll.scrollToTop();
+  };
+
   return (
     <StyledComponentsRegistry>
-      <ReactLenis
-        root
-        easing={(t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t))}
-      >
-        <GlobalStyles />
-        <Preloader setComplete={setComplete} />
-        <div className={complete ? "complete" : "not_complete"}>
-          <Header />
-          {children}
-          <Footer />
+      <GlobalStyles />
+      <Preloader setComplete={setComplete} />
+      <div className={complete ? "complete" : "not_complete"}>
+        <Header />
+        {children}
+        <Footer />
+        {/* زر التمرير إلى الأعلى */}
+        <div onClick={scrollToTop} className="scroll-to-top">
+          Scroll to Top
         </div>
-      </ReactLenis>
+      </div>
     </StyledComponentsRegistry>
   );
 };
